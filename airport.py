2a4f48363f60d66895baf401fbfa68e9e13b69eb
sharukmhdm <sharuk.mhdm@gmail.com>
2026-03-25 11:46:08 +0100

    Update airport.py

--- airport.py
+++ airport.py
@@ -1,197 +1,128 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 23 23:02:17 2026
-
-@author: inesm
-"""
-
 import os
+import matplotlib.pyplot as plt
 
 # --- STEP 1: CLASS DEFINITION ---
 class Airport:
-    def __init__(self, code, lat, lon,):
-        """
-        Constructor for the Airport class.
-        """
+    def __init__(self, code, lat, lon):
         self.code = code
-        self.lat = lat
-        self.lon = lon
-        self.schengen = False  # Default value
+        self.lat = lat  # Sera un nombre décimal (float)
+        self.lon = lon  # Sera un nombre décimal (float)
+        self.schengen = False
 
 def IsSchengenAirport(code):
-    """
-    Checks if the ICAO code belongs to a Schengen country.
-    """
     if not code:
         return False
-    
     # List of prefixes for Schengen countries
-    schengen_codes = [
-        'LO', 'EB', 'LK', 'LC', 'EK', 'EE', 'EF', 'LF', 'ED', 'LG', 'EH', 'LH', 
-        'BI', 'LI', 'EV', 'EY', 'EL', 'LM', 'EN', 'EP', 'LP', 'LZ', 'LJ', 'LE', 
-        'ES', 'LS'
-    ]
+    schengen_codes = ['LO', 'EB', 'LK', 'LC', 'EK', 'EE', 'EF', 'LF', 'ED', 'LG', 'EH', 'LH', 
+                      'BI', 'LI', 'EV', 'EY', 'EL', 'LM', 'EN', 'EP', 'LP', 'LZ', 'LJ', 'LE', 'ES', 'LS']
     return code[:2].upper() in schengen_codes
 
 def SetSchengen(airport):
-    """
-    Sets the Schengen attribute of an airport object.
-    """
     airport.schengen = IsSchengenAirport(airport.code)
 
 def PrintAirport(airport):
-    """
-    Prints airport data to the console.
-    """
     status = "Schengen" if airport.schengen else "Non-Schengen"
-    print(f"ICAO: {airport.code} | Lat: {airport.lat} | Lon: {airport.lon} | Zone: {status}")
+    print(f"ICAO: {airport.code} / Lat: {airport.lat} / Lon: {airport.lon} / Zone: {status}")
 
-# --- STEP 3: FILE & LIST MANAGEMENT ---
+# --- STEP 3: CONVERTER & FILE MANAGEMENT ---
 
+def ConvertToDecimal(coord_str):
+    """ Converts N635906 to decimal degrees """
+    direction = coord_str[0]
+    
+    # IL FAUT METTRE int() ICI POUR TRANSFORMER LE TEXTE EN NOMBRE
+    deg = int(coord_str[1:3])      # "63" -> 63
+    minutes = int(coord_str[3:5])  # "59" -> 59
+    seconds = int(coord_str[5:7])  # "06" -> 6
+    
+    # Maintenant que ce sont des nombres, le calcul fonctionne !
+    decimal = deg + (minutes / 60.0) + (seconds / 3600.0)
+    
+    if direction == 'S' or direction == 'W':
+        decimal = -decimal
+        
+    return round(decimal, 6)
 def LoadAirports(filename):
-    """
-    Reads airports from a text file and returns a list of objects.
-    """
     airports_list = []
+    if not os.path.exists(filename):
+        return []
 
     with open(filename, 'r') as f:
         lines = f.readlines()
-        # Skip header, process each line
-        for line in lines[1:]:
+        
+        if len(lines) <= 1:
+            return []
+
+        for i in range(1, len(lines)):
+            line = lines[i].strip()
+            if not line: continue    
+            
             parts = line.split()
             if len(parts) == 3:
                 code = parts[0]
-                lat = parts[1] # Latitude string
-                lon = parts[2] # Longitude string
-                airports_list.append(Airport(code, lat, lon))
+                
+                # Si ça commence par une lettre (N, S, E, W), on convertit
+                if parts[1][0] in ['N', 'S', 'E', 'W']:
+                    lat_dec = ConvertToDecimal(parts[1])
+                    lon_dec = ConvertToDecimal(parts[2])
+                # Sinon, c'est que c'est DÉJÀ un nombre décimal ! On le transforme juste en float
+                else:
+                    lat_dec = float(parts[1])
+                    lon_dec = float(parts[2])
+                    
+                new_airport = Airport(code, lat_dec, lon_dec)
+                airports_list.append(new_airport)
+                    
     return airports_list
 
 def SaveSchengenAirports(airports, filename):
-    """
-    Saves only Schengen airports to a new file.
-    """
-    # Filter only Schengen airports
-    schengen_list = [a for a in airports if IsSchengenAirport(a.code)]
+    schengen_only = []
+    # Loop to find only Schengen airports
+    for a in airports:
+        if IsSchengenAirport(a.code):
+            schengen_only.append(a)
 
-    if not schengen_list:
-        return -1 # Error: list is empty
+    if len(schengen_only) == 0:
+        return -1 # Error code
     
     with open(filename, 'w') as f:
         f.write("CODE LAT LON\n")
-        for a in schengen_list:
+        for a in schengen_only:
             f.write(f"{a.code} {a.lat} {a.lon}\n")
-    return 0 # Success
+    return 0
 
-def NbSchengenAirports(airports, filename):
-    
-    schengen_list = [a for a in airports if IsSchengenAirport(a.code)]
-    return len(schengen_list)
+# --- STEP 5: GRAPHICS ---
+
+def PlotAirports(airports):
+    nb_s = 0
+    nb_ns = 0
+    # Simple loop to count
+    for a in airports:
+        if IsSchengenAirport(a.code):
+            nb_s = nb_s + 1
+        else:
+            nb_ns = nb_ns + 1
+
+    # Plotting
+    plt.bar("Airports", nb_s, color='blue', label='Schengen')
+    plt.bar("Airports", nb_ns, bottom=nb_s, color='red', label='No Schengen')
+    plt.title("Schengen Airport")
+    plt.ylabel("Count")
+    plt.legend()
+    plt.show()
+
+# --- LIST HELPERS ---
 
 def AddAirport(airports, airport):
-    """
-    Adds an airport to the list if not already present.
-    """
     for a in airports:
         if a.code == airport.code:
-            print(f"Airport {airport.code} already in list.")
-            return
+            return # Already exists
     airports.append(airport)
 
 def RemoveAirport(airports, code):
-    """
-    Removes an airport from the list by its code.
-    """
     for i in range(len(airports)):
         if airports[i].code == code:
             airports.pop(i)
-            return 0 # Success
-    return -1 # Error: not found
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def MapAirports(airports):
-    # This function allows us to see in google earth the airports that we want, distinguished as shengen and non shengen by colors
-    if not airports:
-        return -1
-    try:
-    # We create a KML file to edit it.
-        with open(filename, 'w') as file:
-            file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-            file.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
-            file.write('<Document>\n')
-    # We differentiate from Shengen to non shengen airports with red and blue colors
-            i = 0
-            while i < len(airports):
-                SetSchengen(airports[i])
-                if airports[i].schengen :
-                    pin_color = "ff00ffff"
-                else:
-                    pin_color = "ffff0000"
-    # It generates a Placemark for every airport defining it for google Earth
-                airport = airports[i]
-                file.write('  <Placemark>\n')
-                file.write('    <name>' + airport.code + '</name>\n')
-                file.write('    <Style>')
-                file.write('      <IconStyle>\n')
-                file.write('        <color>' + pin_color + '</color>\n')
-                file.write('        <Icon>\n')
-                file.write('          <href>http://maps.google.com/mapfiles/kml/pushpin/wht-pushpin.png</href>\n')
-                file.write('        </Icon>\n')
-                file.write('      </IconStyle>\n')
-                file.write('    </Style>')
-                file.write('    <Point>\n')
-
-                coords = str(airport.lon) + "," + str(airport.lat)
-                file.write('      <coordinates>' + coords + '</coordinates>\n')
-                file.write('    </Point>\n')
-                file.write('  </Placemark>\n')
-                i += 1
-            file.write('</Document>\n')
-            file.write('</kml>\n')
-        return 0
-    except IOError:
-        return -1
+            return 0
+    return -1
